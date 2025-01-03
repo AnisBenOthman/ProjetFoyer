a7de892b78c890554405f13d73aa10b994837c40
AnisBenOthman <anis12315@live.fr>
2025-01-03 20:17:44 +0100

    correct issues in test repository

--- src/test/java/tn/esprit/tpfoyer/service/BlocServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/service/BlocServiceImplTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class BlocServiceImplTest {
     @Mock
